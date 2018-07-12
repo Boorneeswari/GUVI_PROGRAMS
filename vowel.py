4c318f963b71aa03d59c334823121de8a9b397e4
M.Boorneeswari <34502408+Boorneeswari@users.noreply.github.com>
2018-07-12 15:19:50 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,15 +1,13 @@
-import string
-lis=list(string.ascii_lowercase)
+
+lis=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 vowel=['a','e','i','o','u']
 word=input()
 check=word.lower()
-check=check.split()
+
 for i in check:
     if i in vowel:
         print("Vowel")
     elif i in lis:
-        print("Consonant")
-    elif i=='z':
         print("Consonant")
     else:
         print("invalid")
