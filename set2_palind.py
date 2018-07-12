972eb64dc41031cf9b1ff2f2b9c45f653d64867c
M.Boorneeswari <34502408+Boorneeswari@users.noreply.github.com>
2018-07-12 17:00:53 +0530

    Update set2_palind.py

--- set2_palind.py
+++ set2_palind.py
@@ -1,8 +1,12 @@
-num=int(input())
-strnum=str(num)
-palind=strnum[::-1]
-if strnum== palind:
-    print("ÿes")
+numb=input()
+lis=[]
+num=int(numb)
+while(num!=0):
+    rem=num%10
+    lis.append(str(rem))
+    num=num//10
+lis1=list(numb)
+if lis == lis1:
+    print("yes")
 else:
     print("no")
-    
