2ce67d88f8f40ea02e1061801097cd39b392bbc6
M.Boorneeswari <34502408+Boorneeswari@users.noreply.github.com>
2018-07-12 17:55:10 +0530

    Create set2_prime.py

---
+++ set2_prime.py
@@ -0,0 +1,13 @@
+num=int(input())
+flag=0
+for i in range(2,num):
+    if num%i ==0:
+        flag=1
+        break
+    else:
+        flag=0
+        break
+if flag==1:
+    print("no")
+else:
+    print("yes")
