07c7662b62622f718cc424b1e62be15f3b95feeb
M.Boorneeswari <34502408+Boorneeswari@users.noreply.github.com>
2018-07-12 18:37:20 +0530

    Update set2_primeinterval.py

--- set2_primeinterval.py
+++ set2_primeinterval.py
@@ -2,10 +2,13 @@ num=raw_input()
 num=num.split()
 frst=int(num[0])
 scnd=int(num[1])
-for numb in range(frst,scnd+1):
-    if numb>1:
+
+    
+for numb in range(frst+1,scnd):
+    if numb>1 and numb!=scnd:
         for i in range(2,numb):
             if numb%i==0:
                 break
         else:
             print numb,
+
