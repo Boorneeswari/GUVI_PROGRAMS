2adf0aade1d47269fd012e2a0b0846c70be8dfc4
M.Boorneeswari <34502408+Boorneeswari@users.noreply.github.com>
2018-07-27 18:53:53 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,9 +1,9 @@
-siz=int(input())
-lis=[]
-num=raw_input()
-num=num.split()
-for i in range(siz):
-  lis.append(int(num[i]))
-lis.sort()
-for i in lis:
+sizee=int(input())
+list1=[]
+num1=raw_input()
+num1=num1.split()
+for i in range(sizee):
+  list1.append(int(num1[i]))
+list1.sort()
+for i in list1:
   print i,
