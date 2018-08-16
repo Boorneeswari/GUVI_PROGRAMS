12e3e51077485bbbaf3c069012a2a029058d2028
M.Boorneeswari <34502408+Boorneeswari@users.noreply.github.com>
2018-08-16 18:47:18 +0530

    Create exists.py

---
+++ exists.py
@@ -0,0 +1,13 @@
+num=raw_input()
+num=num.split()
+frst=int(num[0])
+sec=int(num[1])
+inp=raw_input()
+inp=inp.split()
+lis=[]
+for i in range(frst):
+    lis.append(int(inp[i]))
+if sec in lis:
+    print("yes")
+else:
+    print("no")
