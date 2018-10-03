a6863fd135887a6d38dd88d9b62e6baec7e83cfa
M.Boorneeswari <34502408+Boorneeswari@users.noreply.github.com>
2018-10-03 18:15:25 +0530

    Update set7prime.py

--- set7prime.py
+++ set7prime.py
@@ -1,7 +1,6 @@
-num = int(input())
- 
-for i in range(2, int(num/2)):
-    if num % i  == 0:
+num1 = int(input())
+for i in range(2, int(num1/2)):
+    if num1 % i  == 0:
         print("no")
         break
 else:
